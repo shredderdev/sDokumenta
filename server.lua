2c2ced0ce51d39ef17841e5a262d383731be7baf
Shredder <86916008+shredderdev@users.noreply.github.com>
2023-04-07 21:55:54 +0200

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,17 @@
+ESX.RegisterServerCallback("proveriItem", function(source, cb, itemName)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    if xPlayer ~= nil then
+        local item = xPlayer.getInventoryItem(itemName).count
+        if item ~= 0 then 
+            cb(true)
+        else
+            cb(false)
+        end
+    else
+        cb(false)
+    end
+end)
+
+server_scripts {
+    'server.lua'
+}
