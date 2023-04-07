81f50154dadd09c82c1e033970f104e953bdd93d
Shredder <86916008+shredderdev@users.noreply.github.com>
2023-04-07 21:57:14 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,13 @@
 fx_version 'adamant'
-
 game 'gta5'
 
 Creator 'Sredder#9064'
-
 description 'Dokumenta kao itemi i na qtraget pokazivanje'
 
 client_script {
 	'klijent.lua'
+}
+
+server_scripts {
+    'server.lua'
 }
